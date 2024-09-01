dd845f717f46691ca541192c92b1413a77b0e181
dev <dev@pitopito.pf>
2024-08-31 15:13:44 -1000

    Adding bottom buttons

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2614641861"
+    serviceWorkerVersion: "2887989996"
   }
 });
