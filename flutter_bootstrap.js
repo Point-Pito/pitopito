0486fa596acc7a8009dfa8ba9740513ae2cec425
dev <dev@pitopito.pf>
2024-09-27 17:51:06 -1000

    Adding presentation page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1816342092"
+    serviceWorkerVersion: "3895811717"
   }
 });
